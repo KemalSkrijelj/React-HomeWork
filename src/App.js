4a41f89f423fef89e9e4f5f8d5a995a849f2321e
Kemal Škrijelj <kemalskrijelj6@gmail.com>
2024-05-05 21:13:46 +0200

    Start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <div className="text">
+         <h1>Hello</h1>
+
+         <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
